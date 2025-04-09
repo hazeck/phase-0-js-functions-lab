5fe40878da8b510832c94ef22969fe113c2e922d
Chadrick <chadrickalvarez@gmail.com>
2025-04-09 11:10:42 -0400

    Lab Functions in Javascript

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
+function calculateTax (amount) {
+    const taxRate = 0.1;
+    return amount * taxRate;
+}
 
+function convertToUpperCase (text) {
+    return text.toUpperCase();
+}
 
+function findMaximum (num1, num2) {
+    return Math.max(num1, num2);
+}            
+
+function isPalindrome (word) {
+    const reversedWord = word.split('').reverse().join('');
+    return word === reversedWord;
+}
+
+function calculateDiscountedPrice (originalPrice, discount) {
+    return originalPrice - (originalPrice * (discount / 100));
+}
 
 
 
